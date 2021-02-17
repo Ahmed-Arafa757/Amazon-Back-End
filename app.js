474317065b7f7b5b09e7a0655baf7621ad46d094
Serety <serety95@gmail.com>
2021-02-17 19:47:31 +0200

    users updated 17-2-2021

--- app.js
+++ app.js
@@ -61,4 +61,4 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port,()=>console.log("server started at port 3000"));
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,153 +1,118 @@
 var Users = require("../models/usersModal");
-var bodyParser = require("body-parser");
-const { json } = require("body-parser");
-
-
-var jwt = require('jsonwebtoken');
-
-
-
+var jwt = require("jsonwebtoken");
 module.exports = function (app) {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.post("/api/users", function (req, res,next) {
+  
+/////////add new User/////////
+  app.post("/api/users", function (req, res, next) {
     var newUser = new Users({
-        
-        age: req.body.age,
-        name: {
-          first: req.body.name.first,
-          last: req.body.name.last,
-        },
-        userName: req.body.userName,
-        email: req.body.email,
-        phone: req.body.phone,
-        img: req.body.img,
-        address: [
-          {
-            postalCode: req.body.address[0].postalCode,
-            street: req.body.address[0].street,
-            state: req.body.address[0].state,
-            city: req.body.address[0].city,
-            country: req.body.address[0].country,
-            geoMap: {
-              latitude:req.body.address[0].geoMap.latitude,
-              longitude:req.body.address[0].geoMap.longitude,
-            },
-          },
-        ],
-        dateOfRegister: req.body.dateOfRegister,
-      password:req.body.password
-      })
-      
-        
-      newUser.validate(function (err) {
-        if (err) console.log(err);
-        else{
-            Users.create(newUser)
-            .then(users =>res.status(201).send(users))
-            .catch(next)  
-        } 
-      });  
-    
-  });
-/////get all users
-  app.get("/api/users", function (req, res,next) {
-
-    Users.find({})
-        .then(users => res.status(200).send(users))
-        .catch(next)
-
-    // Users.find({}, function (err, users) {
-    //     if (err) throw err;
-    //     res.send(users);
-    // });
-  });
-
-/////get user by name
-  app.get("/api/users/name/:userName", function (req, res,next) {
-   
-    Users.find({userName: req.params.userName})
-    .then(users => res.status(200).send(users))
-    .catch(next)
-    
-    // Users.find(
-    //   {userName: req.params.userName},
-    //   function (err, user) {
-    //     if (err) throw err;
-    //     res.send(user);
-    //   }
-    // );
-  });
-
-/////get user by ID
-  app.get("/api/users/id/:id", function (req, res,next) {
-
-    Users.findById({_id: req.params.id})
-        .then(users => res.status(200).send(users))
-        .catch(next)
-
-    // Users.findById(
-    //   {_id: req.params.id},
-    //   function (err, user) {
-    //     if (err) throw err;
-    //     res.send(user);
-    //   }
-    // );
-  });
-
-  app.patch("/api/users", function (req, res) {
-    res.json({
-        message:"user is Updated"
-    })
-
-    if (req.body._id) {
-      Users.findByIdAndUpdate(
-        req.body.id,
+      age: req.body.age,
+      name: {
+        first: req.body.name.first,
+        last: req.body.name.last,
+      },
+      userName: req.body.userName,
+      email: req.body.email,
+      phone: req.body.phone,
+      img: req.body.img,
+      address: [
         {
-          _id: req.body._id,
-          age: req.body.age,
-          name: {
-            first: req.body.name.first,
-            last: req.body.name.last,
+          postalCode: req.body.address[0].postalCode,
+          street: req.body.address[0].street,
+          state: req.body.address[0].state,
+          city: req.body.address[0].city,
+          country: req.body.address[0].country,
+          geoMap: {
+            latitude: req.body.address[0].geoMap.latitude,
+            longitude: req.body.address[0].geoMap.longitude,
           },
-          userName: req.body.userName,
-          email: req.body.email,
-          phone: req.body.phone,
-          img: req.body.img,
-          address: [
-            {
-              postalCode: req.body.address.postalCode,
-              street: req.body.address.street,
-              state: req.body.address.state,
-              city: req.body.address.city,
-              country: req.body.address.country,
-              geoMap: {
-                latitude: req.body.address.geoMap.latitude,
-                longitude: req.body.address.geoMap.longitude,
-              },
-            },
-          ],
-          dateOfRegister: req.body.dateOfRegister,
         },
-        function (err, user) {
-          if (err) throw err;
-          res.send(user);
-        }
-      );
-    } 
-  });
+      ],
+      dateOfRegister: req.body.dateOfRegister,
+      password: req.body.password,
+    });
 
-  app.delete("/api/users/:id", function (req, res) {
-    //   res.json({
-    //       message:"user is deleted"
-    //   })
-    Users.findByIdAndRemove(req.params.id, function (err) {
-      if (err) throw err;
-      res.send("user deleted");
+    newUser.validate(function (err) {
+      if (err) console.log(err);
+      else {
+        Users.create(newUser)
+          .then((users) => res.status(201).send(users))
+          .then(console.log("user added"))
+          .catch(next);
+      }
     });
   });
+/////////get all users/////////
+  app.get("/api/users", function (req, res, next) {
+    Users.find({})
+      .then((users) => res.status(200).send(users))
+      .catch(next);
 
-// app.listen(3000,()=>console.log("server started at port 3000"));
+  });
+
+/////////get user by name/////////
+  app.get("/api/users/name/:userName", function (req, res, next) {
+    Users.find({ userName: req.params.userName })
+      .then((users) => res.status(200).send(users))
+      .catch(next);
+
+  });
+
+/////////get user by ID/////////
+  app.get("/api/users/id/:id", function (req, res, next) {
+    Users.findById({ _id: req.params.id })
+      .then((users) => res.status(200).send(users))
+      .catch(next);
+ 
+  });
+
+/////////update user by ID/////////
+  app.put("/api/users/:id", function (req, res, next) {
+    const updatedUser = new Users({
+      _id:req.params.id,
+      age: req.body.age,
+      name: {
+        first: req.body.name.first,
+        last: req.body.name.last,
+      },
+      userName: req.body.userName,
+      email: req.body.email,
+      phone: req.body.phone,
+      img: req.body.img,
+      address: [
+        {
+          postalCode: req.body.address[0].postalCode,
+          street: req.body.address[0].street,
+          state: req.body.address[0].state,
+          city: req.body.address[0].city,
+          country: req.body.address[0].country,
+          geoMap: {
+            latitude: req.body.address[0].geoMap.latitude,
+            longitude: req.body.address[0].geoMap.longitude,
+          },
+        },
+      ],
+      dateOfRegister: req.body.dateOfRegister,
+      password: req.body.password,
+    });
+
+    updatedUser.validate(function (err) {
+      if (err) console.log(err);
+      else {
+        Users.updateOne({ _id: req.params.id }, updatedUser)
+      .then(() => Users.findById({ _id: req.params.id }))
+      .then((updatedUser) => res.status(200).send(updatedUser))
+      .then(console.log("user updated"))
+      .catch(next);
+      }
+    });
+ 
+  });
+/////////delete user by ID/////////
+  app.delete("/api/users/:id", function (req, res) {
+    Users.deleteOne({ _id: req.params.id })
+      .then((users) => res.status(204).send(users))
+      .then(console.log("user deleted"));
+      
+  });
+
 };
-
