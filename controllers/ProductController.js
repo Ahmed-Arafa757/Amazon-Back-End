6e5bef67376b3d1829de5c1b912791f6543f571a
amrhossam97 <amrhossam60@hotmail.com>
2021-03-07 04:10:25 +0200

    product by category and sub category

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -54,13 +54,32 @@ module.exports = function (app) {
     })
 
     //get products by seller id
-  app.get('/api/seller/products/:id', function(req,res,next){
-      console.log(req.params.id)
-    Products.find({productSales: req.params.id})
-    .then(products => 
-        {console.log(products);
-        res.status(200).send(products)})
-    .catch(next);
-})
+    app.get('/api/seller/products/:id', function(req,res,next){
+        console.log(req.params.id)
+        Products.find({productSales: req.params.id})
+        .then(products => 
+            {console.log(products);
+            res.status(200).send(products)})
+        .catch(next);
+    })
+
+    //get products by subCategory
+    app.get('/api/products/sub/:sub', function(req,res,next){
+        console.log(req.params.sub)
+        Products.find({productSubCategory: req.params.sub})
+        .then(products => 
+            {console.log(products);
+            res.status(200).send(products)})
+        .catch(next);
+    })
+    //get products by Category
+    app.get('/api/products/category/:category', function(req,res,next){
+        console.log(req.params.category)
+        Products.find({productCategory: req.params.category})
+        .then(products => 
+            {console.log(products);
+            res.status(200).send(products)})
+        .catch(next);
+    })
 
 }
